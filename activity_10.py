331355f837f0ee3156dabb1325d958dfbce563ef
PadmanabhKhunt <you@example.com>
2023-01-27 11:44:31 +0530

    Motion_Detection_Photo_Capturing

--- activity_10.py
+++ activity_10.py
@@ -1,22 +1,73 @@
-import os
+# import os
+# import time
+# from picamera import PiCamera
+
+# FOLDER_NAME = "/home/pi/activity_10"
+
+# if not os.path.exists(FOLDER_NAME):
+#     os.mkdir(FOLDER_NAME)
+
+# camera = PiCamera()
+# camera.resolution = (1280, 720)
+# camera.rotation = 180
+# time.sleep(2)
+
+# counter = 1
+
+# while True:
+#     file_name = FOLDER_NAME + "/img" + str(counter) + ".jpg"
+#     counter += 1
+#     camera.capture(file_name)
+#     print("New photo has been taken")
+#     time.sleep(5)
+
+
+import RPi.GPIO as GPIO
 import time
-from picamera import PiCamera
 
-FOLDER_NAME = "/home/pi/activity_10"
+GPIO.setmode(GPIO.BCM)
+#GPIO_BUZZ = 12
+GPIO_TRIG = 24
+GPIO_ECHO = 23
 
-if not os.path.exists(FOLDER_NAME):
-    os.mkdir(FOLDER_NAME)
-    
-camera = PiCamera()
-camera.resolution = (1280, 720)
-camera.rotation = 180
-time.sleep(2)
+GPIO.setwarnings(False)
+GPIO.setup(GPIO_TRIG, GPIO.OUT)
+GPIO.setup(GPIO_ECHO, GPIO.IN)
+# GPIO.setup(GPIO_BUZZ, GPIO.OUT)
 
-counter = 1
+# buzzer = GPIO.PWM(GPIO_BUZZ, 50)
+# buzzer.start(0)
+while (1):
 
-while True:
-    file_name = FOLDER_NAME + "/img" + str(counter) + ".jpg"
-    counter += 1
-    camera.capture(file_name)
-    print("New photo has been taken")
-    time.sleep(5)
+    GPIO.output(GPIO_TRIG, GPIO.LOW)
+
+    time.sleep(0.5)
+    GPIO.output(GPIO_TRIG, GPIO.HIGH)
+
+    time.sleep(0.00001)
+
+    GPIO.output(GPIO_TRIG, GPIO.LOW)
+
+    while GPIO.input(GPIO_ECHO) == 0:
+
+        start_time = time.time()
+
+    while GPIO.input(GPIO_ECHO) == 1:
+
+        Bounce_back_time = time.time()
+
+    pulse_duration = Bounce_back_time - start_time
+
+    distance = round(pulse_duration * 17150, 2)
+
+    print("distance :" + str(distance) + "cm")
+
+    # for distance in range(0, 101, 1):
+    #     buzzer.ChangeDutyCycle(distance)
+    #     time.sleep(0.1)
+    # for distance in range(100, -1, -1):
+    #     buzzer.ChangeDutyCycle(distance)
+    #     time.sleep(0.1)
+
+
+GPIO.cleanup()
---
+++ bmp_pressure_led.py
@@ -0,0 +1,24 @@
+import bmpsensor
+import RPi.GPIO as GPIO
+import time
+LED_PIN = 23
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(LED_PIN, GPIO.OUT)
+while True:
+    time.sleep(0.01)
+    temp, pressure, altitude = bmpsensor.readBmp180()
+    if temp > 25:
+        GPIO.output(LED_PIN, GPIO.HIGH)
+        time.sleep(2)
+        GPIO.output(LED_PIN, GPIO.LOW)
+    elif (temp < 25):
+        GPIO.output(LED_PIN, GPIO.LOW)
+        
+    print("Temperature is ",temp)  # degC
+    print("Pressure is ",pressure) # Pressure in Pa 
+    print("Altitude is ",altitude) # Altitude in meters
+    print("\n")
+    time.sleep(2)
+    
+GPIO.cleanup() 
